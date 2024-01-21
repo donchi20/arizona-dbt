8675c9459b81f50b9874fc7666f78555dcfe8a36
chichi umelo <cdonchichi@gmail.com>
2024-01-21 12:54:03 +0000

    returns model update

--- models/core/fact_returns.sql
+++ models/core/fact_returns.sql
@@ -20,7 +20,7 @@ fact_returns as
         t.territory_pk as territory_fk,
         p.product_pk as product_fk,
         r.return_quantity,
-        p.product_price * r.return_quantity as return_amt
+        p.product_price * r.return_quantity as return_amount
     
     from returns as r
     left join products as p using (product_id)
