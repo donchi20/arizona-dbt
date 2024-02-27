60e19c5b9a940754d09a10d1cd2d6fd1a67f3db8
fred jimmy <hectorguy20@gmail.com>
2024-02-27 23:48:48 +0000

    commented snapshot

--- snapshots/order_shipping.sql
+++ snapshots/order_shipping.sql
@@ -1,14 +1,14 @@
-{% snapshot order_shipping_snapshot %}
-    {{
-        config(
-        target_database='arizona_staging',
-        target_schema='snapshots',
-          unique_key='order_number',
+-- {% snapshot order_shipping_snapshot %}
+--     {{
+--         config(
+--         target_database='arizona_staging',
+--         target_schema='snapshots',
+--           unique_key='order_number',
 
-          strategy='timestamp',
-          updated_at = 'updated_at'
-        )
-    }}
-    -- Pro-Tip: Use sources in snapshots!
-    select * from {{ source('arizona', 'order_shipping') }}
-{% endsnapshot %}
+--           strategy='timestamp',
+--           updated_at = 'updated_at'
+--         )
+--     }}
+--     -- Pro-Tip: Use sources in snapshots!
+--     select * from {{ source('arizona', 'order_shipping') }}
+-- {% endsnapshot %}
